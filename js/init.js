aab38ca97b275f49c170a917216609bca6aed183
Ball <michael.ball@dynatrace.com>
2017-01-31 10:00:30 +1100

    Added the help menu as a modal

--- js/init.js
+++ js/init.js
@@ -7,3 +7,8 @@ UIupdate = new Event('UIupdate');
 window.addEventListener('UIupdate', function () {
   $('select').material_select();
 });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
