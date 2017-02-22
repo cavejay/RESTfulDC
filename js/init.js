9bf1e90e130276dea08385c5cc3ccc836a2efb29
Ball <michael.ball@dynatrace.com>
2017-02-23 08:26:58 +1100

    LoadingBar now gets fixed to the top of page after scrolling :D

--- js/init.js
+++ js/init.js
@@ -12,3 +12,9 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
 });
+
+$(document).ready(function(){
+  $('#LoadingBar').pushpin({
+    top: 40+35+56
+  });
+});
